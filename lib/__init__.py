4bab38ac1775cde909ebb0528f23f78282b88afa
lann <lann@lannbox.com>
2011-03-29 18:05:50 -0600

    import json into lib

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,10 @@
 import sys
 
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
 sys.path.insert(0, 'lib/beaker')
 sys.path.insert(0, 'lib/bottle')
 sys.path.insert(0, 'lib/gaema')
